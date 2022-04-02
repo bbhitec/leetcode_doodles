000b185a776e85adcc6774a722d103e8b06142b9
mstro _ <bboy.hitec@gmail.com>
2022-04-02 19:18:38 +0300

    add readme template placeholder

--- 347_topk_freq.py
+++ 347_topk_freq.py
@@ -39,77 +39,23 @@ class Solution(object):
         """
 
         # frequency dictionary in format: {key: nums member, value: appearances}
-        dict = {}
+        count_dict = {}
 
         for num in nums:
-            if num in dict:
-                dict[num] = dict[num] + 1
+            if num in count_dict:
+                count_dict[num] = count_dict[num] + 1
             else:
-                dict[num] = 1 # else - add element (not the diff) to hash 
+                count_dict[num] = 1 # else - add element (not the diff) to hash 
 
         # sort by values so the k first members are the most frequent
-        #sortedout = {key: val for key, val in sorted(dict.items(), key = lambda ele: ele[1], reverse = True)}
-        sortedout = sorted(dict.items(), key=lambda x:x[1])
-        sortedoutdict = dict(sortedout)
+        sortedout = {key: val for key, val in sorted(count_dict.items(), key = lambda ele: ele[1], reverse = True)}
+        #sortedout = sorted(count_dict.items(), key=lambda x:x[1])
+        #sortedoutdict = dict(sortedout)
 
-        res = list(sortedoutdict)[0:k]
+        res = list(sortedout)[0:k]
         return res
 
 
-
-
-
-        
-
-
-
-''''''
-#[here] check performances..., use the %time thing
-def perf_count_decorator(func):
-    '''[demo] function performance timer wrapper'''
-
-    def inner(*args, **kwargs):
-        '''decorator inner functions (can access outer functions)'''
-        start = time.perf_counter() # [bp] time the start with performance counter
-        func(*args, **kwargs)
-        diff = time.perf_counter()-start
-        print (f'task {func.__name__} ran for {diff} secs')
-    return inner    # this is the wrapper call
-
-class SolutionZ(object):
-    def twoSumNaive(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        naive: run a nested loop to to check all sum combinations
-        ~6000ms runtime on leetcode
-        """ 
-        for i, vali in enumerate(nums):
-            for j, valj in enumerate(nums):
-                if (i != j): # can't use same element twice
-                    print (f'{i=} {j=} {(vali+valj)=}')
-                    if (vali+valj == target) and (i!=j):
-                        return [i,j]
-
-    def twoSum(self, nums, target):        
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        hash/dictionary solution: use the list value as a member,
-        and its index as a value, while checking difference for every
-        other value (single list pass)
-        """
-        diff_hash = {}
-        # run nums
-        for i, vali in enumerate(nums):
-            diff = target - vali    # calc diff
-            if diff in diff_hash:   # see if matched
-                return [diff_hash[diff], i] # if yes - return indices
-            diff_hash[vali] = i # else - add element (not the diff) to hash   
-        
-        # case of no match found?  (not required since every input has a solution)
 
 # driver
 def main():
@@ -120,16 +66,17 @@ def main():
     # Example 1:
     # Input: nums = [1,1,1,2,2,3], k = 2
     # Output: [1,2]
-    nums = [1,1,1,2,2,3]
+    '''nums = [1,1,1,2,2,3]
     k = 2
     result = s.topKFrequent(nums,k)
-    print (result)
+    print (f'test: {nums=}, {k=}, {result=}')
+    '''
 
 
     nums = [1,1,1,2,2,2,2,2,3,3,3,3,3,3,3,3,3]
     k = 2
     result = s.topKFrequent(nums,k)
-    print (result)
+    print (f'test: {nums=}, {k=}, {result=}')
     
     '''
     sum = 8
--- README.md
+++ README.md
@@ -1,6 +1,36 @@
 # leetcode_doodles
+
+## About
+##### _Optional: extra info that might be relevant_
+
+This is
+Based on [Clear Code's tutorial](https://youtu.be/UZg49z76cLw)
 Various solutions and exercises for leetcode interview prep problems
 
 
+![image alt text title](<image source>https://raw.githubusercontent.com/bbhitec/ncc_recognition_paper/main/ncc_demo.jpg)
+
+
+## Gains, What I've Learned
+- Self-learning and executing the NCC algorithm…
+- Learning …
+- Deriving …
+### Other Gains
+- Conducting research with academic literature
+- MATLAB scripting
+- Academic paper writing and formatting
+
+
+## Usage/How to run
+Run `brochure_test.m` in MATLAB software or
+use it in MATLAB live editor:
+https://www.mathworks.com/products/matlab/live-editor.html
+
+Install the required packages listed:
+```
+pip install -r requirements.txt
+```
+
+
 log:
-2022.01 moved to a new repo with cleanes naming
+2022.01 moved to a new repo with cleaner naming
