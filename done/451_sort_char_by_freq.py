bfb73674bb7a7a32550ace11bcae79894768681d
bbhitec <bboy.hitec@gmail.com>
2022-12-03 12:34:01 +0200

    leetcode 451

---
+++ done/451_sort_char_by_freq.py
@@ -0,0 +1,65 @@
+##!/usr/bin/env python
+'''
+    @author [mst]
+    @file   451_sort_char_by_freq    
+    @brief  leetcode problems series
+    451. Sort Characters By Frequency
+    
+    Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+    Return the sorted string. If there are multiple answers, return any of them.
+
+    gains:
+    -py3 dictionaries, sort by value
+
+    features, changelog:
+    -2022.12: -submission
+
+    @version 0.1 2022.12
+'''
+
+# simply built a char appearance histogram: O(n)
+# sort the histogram by values: O(n)
+# build the resulting string: O(n)
+# sub: 66%T 49%S
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        if s is None:
+            return 0
+
+        buckets = {}
+        output = ""
+
+        # count each char appearance
+        for c in s:
+            if c in buckets:
+                count = buckets.get(c) + 1
+            else:
+                count = 1
+            buckets.update({c:count})
+
+        sorted_buckets = sorted(buckets.items(), key=lambda x:x[1], reverse=True)
+        for pair in sorted_buckets:
+            output = output + (pair[0] * pair[1])
+
+        return output
+        
+        
+
+
+
+
+################## DRIVER
+def main():
+    print ("[mst] 451_sort_char_by_freq")
+    
+    sol = Solution()
+
+       
+    # testing
+    print(sol.frequencySort("tree"))
+    print(sol.frequencySort("cccaaa"))
+    print(sol.frequencySort("Aabb"))
+    
+
+if __name__ == ("__main__"):
+    main()
---
+++ solution_template.md
@@ -0,0 +1,42 @@
+# Intuition
+Build a bucket histogram of appearances of each letter, sort if by the bunber of appearances then construct an output string by printing char*times_appeared
+
+# Approach
+Use a dictionary to hold the histogram where key is char and value is the number of appearances. Once built, sort by value and build a string with given data
+
+# Complexity
+- Time complexity:
+    - Char appearance histogram: O(n)
+    - Sort the histogram by values: O(n*log(n))
+    - built she resulting string: O(n)
+
+- Space complexity:
+    - O(a) to keep character appearances counts (a is alphabet size)
+    - O(n) to sort the histogram
+    - O(n) to build the result string
+- At time of submission: 66% Time, 49% Space ratings
+
+# Code
+```
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        if s is None:
+            return 0
+
+        buckets = {}
+        output = ""
+
+        # count each char appearance
+        for c in s:
+            if c in buckets:
+                count = buckets.get(c) + 1
+            else:
+                count = 1
+            buckets.update({c:count})
+
+        sorted_buckets = sorted(buckets.items(), key=lambda x:x[1], reverse=True)
+        for pair in sorted_buckets:
+            output = output + (pair[0] * pair[1])
+
+        return output
+```
