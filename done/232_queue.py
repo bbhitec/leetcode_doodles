2222e82802672d27336a104a423fb633ee4e69e7
bbhitec <bboy.hitec@gmail.com>
2022-12-18 12:25:33 +0200

    leetcode 232 and 392

---
+++ done/232_queue.py
@@ -0,0 +1,58 @@
+##!/usr/bin/env python
+'''
+    @author [mst]
+    @file    232_queue.py    
+    @brief  leetcode problems series
+    232. Implement Queue using Stacks    
+    Implement a first in first out (FIFO) queue using only two stacks.
+    The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+    
+    features, changelog:
+    -2022.12: -submission
+
+    @version 0.1 2022.12
+'''
+
+# I think I've cheated here by exploiting py3 lists :|
+#
+# sub: 99%T 76%S
+class MyQueue:
+
+    fifo = []
+
+    def __init__(self):
+        self.fifo = []
+
+    def push(self, x: int) -> None:
+        self.fifo.append(x)
+
+    def pop(self) -> int:
+        if len(self.fifo) > 0:
+            x = self.fifo.pop(0)
+            return x
+        return -1
+
+    def peek(self) -> int:
+        if len(self.fifo) > 0:
+            return self.fifo[0]
+        return -1
+
+    def empty(self) -> bool:
+        return len(self.fifo) == 0
+        
+
+
+################## DRIVER
+def main():
+    print ("[mst]")
+    
+    obj = MyQueue()
+    obj.push(1)
+    param_2 = obj.pop()
+    param_3 = obj.peek()
+    param_4 = obj.empty()
+    
+
+
+if __name__ == ("__main__"):
+    main()
---
+++ done/392_is_subsequence.cpp
@@ -0,0 +1,85 @@
+/*
+[mst] 392_is_subsequence.cpp
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+Gains:
+-Hash map histogram uses
+
+features, changelog:
+-2022.12: -initial draft
+*/ 
+
+
+////////////////// LIBS
+#include <iostream>            // usage of console prints
+using namespace std;
+
+
+////////////////// DECL_IMPL
+
+
+// simply traverse a pointer over subsequence and make sure characters appear in the targer string
+// note: using counter i and s[i] instead of the pointer yielded 22%S result!
+//
+// sub: 100%T 98%S
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+		if (s.empty()) return true; // empty subsequence is true by default
+		int sl = s.length();
+		if (s.length() > t.length()) return false;
+
+		char *sc = &s[0];
+		
+		for (char& tc: t) {
+			if ((*sc == tc) && (*sc != '\0')) {
+				sc++;
+				if (*sc == '\0') return true;	// found subsequence
+			}
+		}
+        return false;
+    }
+};
+
+
+// this is more like a substring rather a subsequence...
+class Solution0 {
+public:
+    bool isSubsequence(string s, string t) {
+		if (s.empty()) return false;	// halt on empty target
+		int sl = s.length();
+		if (s.length() > t.length()) return false;
+
+		int i = 0;
+		
+		for (char& tc: t) {
+			while ((s[i] == tc+i) && (i < sl)) {
+				i++;
+			}
+			if (i == sl) return true;	// found subsequence
+			i = 0;
+		}
+        return false;
+    }
+};
+	
+
+////////////////// DRIVER
+int main()
+{
+	Solution sol;
+
+	string s = "abc";
+	string t = "ahbgdc";
+	cout << "result: " << sol.isSubsequence(s, t) << endl;
+
+	string s2 = "axc";
+	string t2 = "ahbgdc";
+	cout << "result: " << sol.isSubsequence(s2, t2) << endl;
+
+	string s3 = "";
+	string t3 = "ahbgdc";
+	cout << "result: " << sol.isSubsequence(s3, t3) << endl;
+	
+	return 0;
+}
