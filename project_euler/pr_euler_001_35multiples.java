22a0b698bb0fb9465214c0502f9543cb226824e2
bbhitec <bboy.hitec@gmail.com>
2023-01-18 23:22:31 +0200

    importing project euler progress

---
+++ project_euler/pr_euler_001_35multiples.java
@@ -0,0 +1,25 @@
+/*
+ * project euler question
+ */
+
+package pe001_35mult;
+
+import java.io.Console;
+
+public class Pe001_35mult {
+
+	public static void main(String[] args) {
+		int i;
+		int sum = 0;
+		for (i = 0; i< 1000; i++)
+		{
+			if ((0 == i % 3) || (0 == i%5)){
+				sum += i;				
+			}
+				
+		}
+		System.out.println("Multiples of 3 and 5, Problem 1, sum = " + sum);
+
+	}
+
+}
