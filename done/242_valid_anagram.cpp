37e0dd0c9dea23cc2f70f7b849f3553af7bfb15a
bbhitec <bboy.hitec@gmail.com>
2023-02-05 12:27:32 +0200

    leetcode 242 in cpp, better time

---
+++ done/242_valid_anagram.cpp
@@ -0,0 +1,78 @@
+/**
+    @author [mst]
+    @brief  leetcode problems series
+    242_valid_anagram
+    Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+    An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+	gains:
+    -cpp string and char handling
+    -histogram mapping and checking
+
+    features, changelog:
+    -2023.01:	-initial draft
+
+    @version 0.1 2023.01
+*/
+
+
+
+////////////////// LIBS
+#include <iostream>            // usage of console prints
+#include <vector>
+
+using namespace std;
+
+
+////////////////// DECL_IMPL
+
+
+// build occurence histograms/maps for the given strings and compare the results
+//
+// sub: 95T 61S
+class Solution {
+private:
+    // safe case ASCII chars limit
+	static const int MAX = 256;
+public:
+    bool isAnagram(string s, string t) {
+        int s_size = s.length();
+		int t_size = t.length();
+
+        // basic cases
+        if (s.empty() || t.empty() || t_size == 0 || s_size != t_size) {
+            return false;
+        }
+
+        // map occurences (we presume only valid ASCII chars are given)
+        int sMap[MAX] = {0};
+        int tMap[MAX] = {0};
+        for (int i = 0; i < s_size; i++) {
+            sMap[s[i] - 'a']++;
+            tMap[t[i] - 'a']++;
+        }
+
+        // compare histogram maps
+        for (int i = 0; i < MAX; i++)
+            if (tMap[i] != sMap[i]) return false;
+
+        return true;
+    }
+};
+
+
+
+////////////////// DRIVER
+int main()
+{
+	Solution sol;
+
+	// string s = "anagram", t = "nagaram";
+    string s = "a", t = "ab";
+
+	cout << "res: " << sol.isAnagram(s,t) << endl;
+
+
+
+	return 0;
+}
