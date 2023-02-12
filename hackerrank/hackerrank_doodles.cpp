33dbd5a385d43b5364997183578c5083e2655e01
bbhitec <bboy.hitec@gmail.com>
2023-02-12 18:38:53 +0200

    hacker rank divisable pairs init

--- hackerrank/hackerrank_doodles.cpp
+++ hackerrank/hackerrank_doodles.cpp
@@ -420,6 +420,19 @@ string catAndMouse(int x, int y, int z) {
     return "";
 }
 
+
+int divisibleSumPairs(int n, int k, vector<int> ar) {
+    int count = 0;
+    for(int i =0 ; i < n -1 ; i++){
+        for(int j=i+1 ; j < n ; j++){
+            if( (ar[i]+ar[j])%k ==0){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
 ////////////////// DRIVER
 int main() {
 	cout << "[mst] hackerrank doodle" << endl;
