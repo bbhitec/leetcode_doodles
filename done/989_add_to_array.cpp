5b6604200034d1e39ad60cee00722043c72bc4bf
bbhitec <bboy.hitec@gmail.com>
2023-02-15 22:45:26 +0200

    leetcode 989

---
+++ done/989_add_to_array.cpp
@@ -0,0 +1,81 @@
+/**
+    @author [mst]
+    @brief  leetcode problems series
+    989_add_to_array
+    The array-form of an integer num is an array representing its digits in left to right order.
+    For example, for num = 1321, the array form is [1,3,2,1].
+    Given num, the array-form of an integer, and an integer k, return the array-form of the integer num + k.
+
+    gains:
+    -
+
+    @version 0.1 2023.02
+*/
+
+
+
+////////////////// LIBS
+#include <iostream>            // usage of console prints
+#include <vector>
+#include <deque>
+#include <string>
+using namespace std;
+
+
+////////////////// DECL_IMPL
+
+// simple conditional adding
+//
+// sub?: 68T 40S
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        deque<int> res;
+        int carry = 0;
+        int sum = 0;
+        int d = 0;
+        int num_d = 0;
+        bool in_num=true;
+
+        // [demo] reverse iteration
+        auto it = num.rbegin();
+        while (in_num || k) {
+            d = k%10;
+            k = k/10;
+            num_d = (in_num)?*it:0;
+
+            sum = num_d + d + carry;
+            carry = sum/10;
+
+            res.push_front(sum%10);
+
+            it++;
+            if (it == num.rend()) in_num = false;
+        }
+
+        // handle leftovers
+        if (carry) res.push_front(1);
+
+        return {res.begin(),res.end()};
+    }
+};
+
+
+
+////////////////// DRIVER
+int main()
+{
+	Solution sol;
+    // vector<int> num = {1,2,0,0}; int  k = 34;
+    vector<int> num = {6}; int  k = 809;
+    // vector<int> num = {0}; int  k = 23;
+    // vector<int> num = {9,9,9,9,9,9,9,9,9,9}; int  k = 1;
+
+    cout << "res: " << endl;
+    vector<int> res = sol.addToArrayForm(num,k);
+
+    for (auto n : res) cout << n << " ";
+
+
+	return 0;
+}
