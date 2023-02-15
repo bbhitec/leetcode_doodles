7ac7a0196be418d464e8b68a76e18364f95d0530
bbhitec <bboy.hitec@gmail.com>
2023-02-15 22:46:50 +0200

    hacker rank merge point solution

--- hackerrank/hackerrank_doodles.cpp
+++ hackerrank/hackerrank_doodles.cpp
@@ -421,6 +421,53 @@ string catAndMouse(int x, int y, int z) {
 }
 
 
+// Hacker Rank finding a merge point of two lists solution
+// https://www.hackerrank.com/challenges/find-the-merge-point-of-two-joined-linked-lists/problem
+struct SinglyLinkedListNode {
+    int data;
+    SinglyLinkedListNode* next;
+};
+
+int findMergeNode(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+    SinglyLinkedListNode *la = head1;
+    SinglyLinkedListNode *sm = head2;
+    int l1=0,l2=0;
+    while(head1!=NULL) {
+        l1++;
+        head1 = head1->next;
+    }
+    head1 = la;
+    while(head2!=NULL) {
+        l2++;
+        head2 = head2->next;
+    }
+    head2 = sm;
+    if(l1>l2) {
+        la = head1;
+        sm = head2;
+    }
+    else {
+        la = head2;
+        sm = head1;
+    }
+    int f = 0;
+    l1 = abs(l1-l2);
+    for (int i = 0; i < l1; i++) {
+        la = la->next;
+    }
+    while (la != NULL && sm != NULL) {
+        if(la == sm) {
+            f=la->data;
+            break;
+        }
+        la = la->next;
+        sm = sm->next;
+    }
+    return f;
+
+}
+
+
 int divisibleSumPairs(int n, int k, vector<int> ar) {
     int count = 0;
     for(int i =0 ; i < n -1 ; i++){
