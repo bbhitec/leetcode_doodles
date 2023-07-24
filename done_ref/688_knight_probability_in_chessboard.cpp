8fb1a0a60b152831b750b8db0f4e409e35d1cdf5
bbhitec <bboy.hitec@gmail.com>
2023-07-24 20:28:39 +0300

    leetcode 688 cpp ref

--- done_ref/688_knight_probability_in_chessboard.cpp
+++ done_ref/688_knight_probability_in_chessboard.cpp
@@ -30,8 +30,7 @@ using namespace std;
 ////////////////// DECL_IMPL
 
 
-
-class Solution {
+class Solution2 {
 public:
     int onBoard = 0;
     bool withinBoundaries (int n, int row, int col) {
@@ -42,18 +41,60 @@ public:
     double knightProbability(int n, int k, int row, int col) {
       if (k < 0) return 0;
 
-      return withinBoundaries(row, col) +
-      knightProbability (n, k-1, row+1 col+2) +
-      knightProbability (n, k-1, row-1 col+2) +
-      knightProbability (n, k-1, row+1 col-2) +
-      knightProbability (n, k-1, row-1 col-2) +
-      knightProbability (n, k-1, row+2 col+1) +
-      knightProbability (n, k-1, row-2 col+1) +
-      knightProbability (n, k-1, row+2 col-1) +
-      knightProbability (n, k-1, row-2 col-1) +
+      return withinBoundaries(n, row, col) +
+      knightProbability (n, k-1, row+1, col+2) +
+      knightProbability (n, k-1, row-1, col+2) +
+      knightProbability (n, k-1, row+1, col-2) +
+      knightProbability (n, k-1, row-1, col-2) +
+      knightProbability (n, k-1, row+2, col+1) +
+      knightProbability (n, k-1, row-2, col+1) +
+      knightProbability (n, k-1, row+2, col-1) +
+      knightProbability (n, k-1, row-2, col-1);
     }
 };
 
+
+// snipped reference dp solution
+//
+// sub 63 34
+class Solution
+{
+  public:
+    double knightProbability(int n, int K, int r, int c)
+    {
+      constexpr double kProb = 0.125; //
+      const vector<pair<int, int>> dirs{{-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}};
+
+      // dp[i][j] := probability to stand on (i, j)
+      vector<vector<double>> dp(n, vector<double>(n));
+      dp[r][c] = 1.0;
+
+      for (int k = 0; k < K; ++k)
+      {
+        vector<vector<double>> newDp(n, vector<double>(n));
+        for (int i = 0; i < n; ++i)
+          for (int j = 0; j < n; ++j)
+            if (dp[i][j] > 0.0)
+            {
+              for (const auto &[dx, dy] : dirs)
+              {
+                const int x = i + dx;
+                const int y = j + dy;
+                if (x < 0 || x >= n || y < 0 || y >= n) // out of bounds
+                  continue;
+                newDp[x][y] += dp[i][j] * kProb;
+              }
+            }
+        dp = move(newDp);
+      }
+
+      return accumulate(dp.begin(), dp.end(), 0.0,
+                        [](double s, const vector<double> &row)
+                        {
+                          return s + accumulate(row.begin(), row.end(), 0.0);
+                        });
+    }
+};
 
 ////////////////// DRIVER
 int main()
