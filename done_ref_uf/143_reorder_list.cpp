49fb698641742901d12706105e31ad171031fc6b
bbhitec <bboy.hitec@gmail.com>
2024-05-07 23:59:56 +0300

    leetcode 143 init

---
+++ done_ref_uf/143_reorder_list.cpp
@@ -0,0 +1,152 @@
+/**
+    @author [mst]
+    @brief  leetcode series
+    143_reorder_list
+    You are given the head of a singly linked-list. The list can be represented as:
+
+    L0 → L1 → … → Ln - 1 → Ln
+    Reorder the list to be on the following form:
+
+    L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+    You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+    gains:
+    -rd
+    -run examples
+
+
+    @version 0.1 2023.06
+*/
+
+
+
+////////////////// LIBS
+#include <iostream>     // usage of console prints
+#include <vector>
+#include <algorithm>    // usage of sort
+#include <cstring>      // use of memset
+#include <numeric>      // use of accumulate
+using namespace std;
+
+
+////////////////// DECL_IMPL
+
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+// print a list helper method
+void printList (ListNode* head) {
+    if (head == nullptr) {
+        std::cout << "empty list" << std::endl;
+        return;
+    }
+
+    ListNode* lp = head;
+    while (lp) {
+        std::cout << lp->val << ',';
+        lp = lp->next;
+    }
+
+    std::cout << "nullptr" << '\n';
+}
+
+// list push helper function
+void pushList(ListNode* &head, int val) {
+    // [bp] this will still work with an empty list case
+    ListNode* NewNode = new ListNode(val, head);
+    head = NewNode;
+}
+
+// list end insertion helper function
+void addTailList(ListNode* &head, int val) {
+    //if (head == nulptr) { // [demo] unnecessary check
+    //    return
+    //}
+    ListNode* NewNode = new ListNode(val, nullptr);
+    if (head == nullptr) {
+        head = NewNode;
+        return;
+    }
+
+    ListNode* ptr = head;
+    while (ptr->next) {
+        ptr = ptr->next;
+    }
+    ptr->next = NewNode;
+}
+
+
+// to be rd
+// assisted
+//
+// sub 56, 55
+class Solution {
+public:
+  void reorderList(ListNode* head) {
+    if (!head || !head->next)
+      return;
+
+    ListNode* mid = findMid(head);
+    ListNode* reversed = reverse(mid);
+    merge(head, reversed);
+  }
+
+private:
+  ListNode* findMid(ListNode* head) {
+    ListNode* prev = nullptr;
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while (fast != nullptr && fast->next != nullptr) {
+      prev = slow;
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+    prev->next = nullptr;
+
+    return slow;
+  }
+
+  ListNode* reverse(ListNode* head) {
+    ListNode* prev = nullptr;
+    ListNode* curr = head;
+
+    while (curr) {
+      ListNode* next = curr->next;
+      curr->next = prev;
+      prev = curr;
+      curr = next;
+    }
+
+    return prev;
+  }
+
+  void merge(ListNode* l1, ListNode* l2) {
+    while (l2) {
+      ListNode* next = l1->next;
+      l1->next = l2;
+      l1 = l2;
+      l2 = next;
+    }
+  }
+};
+
+////////////////// DRIVER
+int main()
+{
+    Solution s;
+
+    // string res = s.reverseWords("Let's take LeetCode contest");
+    cout << "";
+
+
+    return 0;
+}
